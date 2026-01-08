0173e25b63240f6eeec30db2cd0f953b5cfb952f
vV29Raghav <vvrma923@gmail.com>
2026-01-08 17:55:15 +0530

    Update gemini model

--- socketServer/controllers/codeController.js
+++ socketServer/controllers/codeController.js
@@ -25,7 +25,7 @@ exports.executeCode = async (req, res) => {
     `;
 
         const response = await ai.models.generateContent({
-            model: 'gemini-1.5-flash',
+            model: 'gemini-2.5-flash',
             contents: [{ role: 'user', parts: [{ text: prompt }] }],
         });
 
